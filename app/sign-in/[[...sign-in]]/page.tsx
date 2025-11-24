dda5e1cd72bb8805a56935b2538868f700671a59
unknown <harshgoel2004@gmail.com>
2025-11-25 00:26:36 +0530

    thrid commit

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,10 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignIn />
+  return (
+    <div className='w-screen h-screen flex items-center justify-center bg-black/30' style={{backgroundImage: 'url(/bg.png)', backgroundBlendMode: 'overlay', backgroundSize: 'cover'}}>
+      <SignIn />
+
+    </div>
+  )
 }
